67351b8d38449ed4181c4d08f89f33939b4390a4
Wajahat Karim <wajahatkarim3@gmail.com>
2020-03-13 18:24:46 +0500

    Some more changes to fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@ node -v
 hugo version
 
 # Get Firebase Token
-FIREBASE_TOKEN = $1
-echo $FIREBASE_TOKEN
+if [ -z "${FIREBASE_TOKEN}" ]; then
+    echo "FIREBASE_TOKEN is missing"
+    exit 1
+fi
 
 # Generate hugo site
 hugo
 
 # Deply it on Firebase
-firebase use --token $FIREBASE_TOKEN
-firebase deploy --non-interactive --token $FIREBASE_TOKEN
+firebase use --token "${FIREBASE_TOKEN}"
+firebase deploy --non-interactive --token "${FIREBASE_TOKEN}"
